cd22bb0b6c729ad2daecedb8adeaaf28c5f8167b
BGE31-Thomas <tpjarles@gmail.com>
2024-07-26 16:32:38 +0200

    Premier commit

--- src/Entity/DelImageVote.php
+++ src/Entity/DelImageVote.php
@@ -50,11 +50,120 @@ class DelImageVote
     private $valeur;
 
     /**
-     * @var \DateTime
+     * @var \DateTimeImmutable
      *
      * @ORM\Column(name="date", type="datetime", nullable=false)
      */
     private $date;
 
+    
 
+
+    /**
+     * Get the value of idVote
+     */
+    public function getIdVote(): int
+    {
+        return $this->idVote;
+    }
+
+    /**
+     * Set the value of idVote
+     */
+    public function setIdVote(int $idVote): self
+    {
+        $this->idVote = $idVote;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ceImage
+     */
+    public function getCeImage(): int
+    {
+        return $this->ceImage;
+    }
+
+    /**
+     * Set the value of ceImage
+     */
+    public function setCeImage(int $ceImage): self
+    {
+        $this->ceImage = $ceImage;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ceProtocole
+     */
+    public function getCeProtocole(): int
+    {
+        return $this->ceProtocole;
+    }
+
+    /**
+     * Set the value of ceProtocole
+     */
+    public function setCeProtocole(int $ceProtocole): self
+    {
+        $this->ceProtocole = $ceProtocole;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of ceUtilisateur
+     */
+    public function getCeUtilisateur(): string
+    {
+        return $this->ceUtilisateur;
+    }
+
+    /**
+     * Set the value of ceUtilisateur
+     */
+    public function setCeUtilisateur(string $ceUtilisateur): self
+    {
+        $this->ceUtilisateur = $ceUtilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of valeur
+     */
+    public function isValeur(): bool
+    {
+        return $this->valeur;
+    }
+
+    /**
+     * Set the value of valeur
+     */
+    public function setValeur(bool $valeur): self
+    {
+        $this->valeur = $valeur;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of date
+     */
+    public function getDate(): \DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set the value of date
+     */
+    public function setDate(\DateTimeImmutable $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
 }
